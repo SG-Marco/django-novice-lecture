65df26ceda8dc021305a9e83d5ae62ad470cb1aa
SG-Marco <rollingpoe@gmail.com>
2022-06-27 22:24:23 +0900

    prefetch

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,3 +1,6 @@
+from django.core.paginator import Page, Paginator
+from django.db.models import QuerySet
+
 from tabom.models import Article
 
 
@@ -5,5 +8,9 @@ def get_an_article(article_id: int) -> Article:
     return Article.objects.filter(id=article_id).get()
 
 
+def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+    return Article.objects.order_by("-id").prefetch_related("like_set")[offset : offset + limit]
 
 
+def get_article_page(page: int, limit: int) -> Page:
+    return Paginator(Article.objects.order_by("-id"), limit).page(page)
